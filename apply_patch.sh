e823c863d67a2eb9a761f811d3e578139e18a669
shtnpd <pd.ishutina@gmail.com>
2025-05-29 16:10:57 +0000

    small changes

--- apply_patch.sh
+++ apply_patch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SITE_PACKAGES=$(python3 -c "import sysconfig; print(sysconfig.get_paths()['purelib'])")
-mv patches/core.py $SITE_PACKAGES/slices/
+cp patches/core.py $SITE_PACKAGES/slices/
